931197a8cf1f16bce51b1ddd47edf09aaf03da6f
trickydisco78 <Gavin.Thomas@uwe.ac.uk>
2016-11-23 11:40:34 +0000

    add courses route

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 // This component handles the App template used on every page
 
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render() {
-   
+
     return (
       <div className="container-fluid">
-      <p>header here...</p>
+      <Header />
       {this.props.children}
       </div>
     );
