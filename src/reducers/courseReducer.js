0e2e19f780b2891afed7d5e4c7fe8f2c3c8bad21
trickydisco78 <Gavin.Thomas@uwe.ac.uk>
2017-02-14 16:49:26 +0000

    document what a reducer is

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 //Take state and action and return new state
+//The reducer consolidates actions into a single object representing application state.
+//. Each time a redux reducer is called, the state is passed in with the action (state, action).
+// This state is then reduced (or accumulated) based on the action, and then the next state is returned
 
 export default function courseReducer(state = [], action) {
   switch (action.type) {
     case "CREATE_COURSE":
-      //Use spread operator (new instance of state array) and copy course to empty object
       return [...state, Object.assign({}, action.course)];
-    //DO NOT MUTATE STATE
-    //state.push(action.course);
-    //return state;
-=======
-export default function courseReducer(state = [], action) {
-  switch (action.type) {
-      case 'CREATE_COURSE' :
-        return [...state,
-          Object.assign({}, action.course)
-        ];
->>>>>>> 7a51ff05f6c43f222b4416aa9105ad3ad3604841
 
     default:
       return state;
