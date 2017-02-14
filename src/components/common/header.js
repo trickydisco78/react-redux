1209743bc646ed8e2e25cb299e789734ac2f30d3
trickydisco78 <Gavin.Thomas@uwe.ac.uk>
2017-02-14 16:45:08 +0000

    single quotes

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link, IndexLink } from "react-router";
+import React, {PropTypes} from 'react';
+import {Link, IndexLink} from 'react-router';
 
 const Header = () => {
   return (
@@ -7,10 +7,9 @@ const Header = () => {
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
       <Link to="/about" activeClassName="active">About</Link>
-      {" | "}
-      <Link to="/courses" activeClassName="active">Courses</Link>
+       {" | "}
+       <Link to="/courses" activeClassName="active">Courses</Link>
     </nav>
   );
 };
-
 export default Header;
